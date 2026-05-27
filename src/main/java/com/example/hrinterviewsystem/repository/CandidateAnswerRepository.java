283b4267a902ab82e764fbfe9283b44f8f9d0923
VershininaBrp <oleg.vershinin.2000@icloud.com>
2026-05-28 00:52:53 +0500

    y

--- src/main/java/com/example/hrinterviewsystem/repository/CandidateAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hrinterviewsystem.repository;
-
-import com.example.hrinterviewsystem.entity.CandidateAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateAnswerRepository extends JpaRepository<CandidateAnswer, Long> {
-}
